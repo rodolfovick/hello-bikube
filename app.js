736b9b424d14b4925a807dd5415a25a646099f76
Rodolfo Vick <rodolfo.vick@gmail.com>
2025-07-18 16:27:59 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello Bikubelabs! Welcome to Hackaton\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
